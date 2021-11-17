a7cb2897fa443c474dd54408eaa2dfe5b3c87814
FeNToNelele <91262730+FeNToNelele@users.noreply.github.com>
2021-11-17 14:12:48 +0000

    Web routeok módosítva

--- Questionnaire/routes/web.php
+++ Questionnaire/routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
 use App\Http\Controllers\PagesController;
-use App\Http\Controllers\QuizController;
 use App\Http\Controllers\SettingsController;
-use App\Http\Controllers\LeaderboardController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 Route::resource('/settings', SettingsController::class);
 
-Route::get('/', [PagesController::class, 'index']);
+Route::get('/', [PagesController::class, 'index'])->name('index');
 
 Route::get('/create', [SettingsController::class, 'create']) -> name('create');
 
@@ -18,20 +16,16 @@ Route::get('/create/store', function () {
     return redirect('/create');
 });
 
-Route::post('/delete', [SettingsController::class, 'destroy']) -> name('question.destroy');
-Route::get('/delete', function () {
-    return redirect('/settings');
-});
+Route::get('/create/addquestions/{id}', [SettingsController::class, 'add']);
+Route::post('/create/addquestions/{id}/store', [SettingsController::class, 'storeQuestions']);
+
+Route::get('/settings/{id}/update', [SettingsController::class, 'update']);
+Route::post('/settings/{id}/update/store', [SettingsController::class, 'updateQuestions']);
+
+
+Route::post('/delete/questionnaire', [SettingsController::class, 'destroyQuestionnaire']);
+Route::post('/delete/question', [SettingsController::class, 'destroyQuestion']);
 
 Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
-
-Route::post('/quiz', [QuizController::class, 'index'])->name('quiz');
-Route::get('/quiz', function () { return redirect('/'); } );
-
-Route::post('/quiz/evaluation', [QuizController::class, 'evaluation'])->name('evaluation');
-Route::get('/quiz/evaluation', function () { return redirect('/'); });
-
-Route::get('/leaderboards', [LeaderboardController::class, 'index'])->name('leaderboards');
-Route::post('/leaderboards', [LeaderboardController::class, 'index']);
 
 Auth::routes();
