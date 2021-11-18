fc63f8edd3bc75a4b1480087fbbad22100885e7c
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-11-18 10:17:32 +0100

    PagesController - Kérdőív megjelenítési adatok BE

--- Questionnaire/app/Http/Controllers/PagesController.php
+++ Questionnaire/app/Http/Controllers/PagesController.php
@@ -8,7 +8,36 @@ use Illuminate\Support\Facades\DB;
 class PagesController extends Controller
 {
     public function index() {
-        $numberOfQuestions = DB::table('kerdoivs')->count('kerdoiv_id');
-        return view('pages.index', compact('numberOfQuestions'));
+        $numberOfQuestionnaire = DB::table('kerdoivs')->count('kerdoiv_id');
+        if($numberOfQuestionnaire != 0)
+        {
+            $questionnaire = DB::table('kerdoivs')
+            ->select('*')
+            ->orderby('kerdoiv_id')->first();
+
+            $questionnaire_id = $questionnaire->kerdoiv_id;
+            $questions = DB::table('kerdeseks')
+            ->select('*')
+            ->where('kerdoiv_id', '=',  $questionnaire_id)->get();
+
+            $answers_arr = [];
+            foreach($questions as $question)
+            {
+                $question_id = $question->kerdes_id;
+                $answers = DB::table('valaszoks')
+                ->select('*')
+                ->where('kerdes_id', '=', $question_id)->get();
+                array_push($answers_arr, $answers);
+            }
+            return view('pages.index', compact('numberOfQuestionnaire'))
+            ->with('questionnaire',$questionnaire)
+            ->with('questions',$questions)
+            ->with('answers_arr',$answers_arr);
+        }
+        else{
+            return view('pages.index', compact('numberOfQuestionnaire'));
+        }
+       
     }
 }
+        
