c68cbe7a66501958c643a568a875497494871e31
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-12-02 10:47:48 +0100

    jQuery + Ajax futtatáshoz szükséges <script> include-olása az univerzális header-be

--- Questionnaire/resources/views/layouts/app.blade.php
+++ Questionnaire/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Scripts -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="{{ asset('js/controllers.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     
