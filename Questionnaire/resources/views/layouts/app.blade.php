fe1dc397aec00d8f84d4e3628b67a266c1fce64a
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-12-02 10:44:16 +0100

    controllers.js include-olása a header-ben

--- Questionnaire/resources/views/layouts/app.blade.php
+++ Questionnaire/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Scripts -->
+    <script src="{{ asset('js/controllers.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     
     <!-- Styles -->
