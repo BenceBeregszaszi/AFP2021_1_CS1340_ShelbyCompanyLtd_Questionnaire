97e4224badc6e3584a5f9f1bbc6c7dec59c5e429
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-12-02 10:46:37 +0100

    Kérdőív/Kérdés szöveg módosító input hide/show function
    lefutáshoz szükséges a jQuery importálása a header-be

--- Questionnaire/resources/js/controllers.js
+++ Questionnaire/resources/js/controllers.js
@@ -1,1 +1,12 @@
+const { random } = require("lodash");
+
+$(document).ready(function(){
+  
+  //Kérdőívek illetve kérdések szövegének módosítására szolgáló megjelenítés
+  $("#changerButton").click(function(){
+    $("#cim").hide();
+    $("#cimChanger").show();
+  })
+  
+}
 
