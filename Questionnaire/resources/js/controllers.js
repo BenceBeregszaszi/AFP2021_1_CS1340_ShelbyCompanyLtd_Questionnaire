49e1e2f46cd6b6542481c534851cc77fc3cff16f
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-12-09 14:35:24 +0100

    Statisztika grafikonjait megjelenítő function megírása

--- Questionnaire/resources/js/controllers.js
+++ Questionnaire/resources/js/controllers.js
@@ -14,6 +14,191 @@ $(document).ready(function(){
     navigator.clipboard.writeText(link);
     console.log(link);
   })
+    
+  //Statiszika weboldalon megjelenő grafikonok megjelenítése
+  $(".questionBtn").click(function(){
+    $(".questionBtn").css({'color': 'black', "background-color": "white"});
+    $(this).css({'color': '#4d2600', "background-color": "rgba(194, 194, 163,0.5)"});
+
+    const params = $(this).data();
+    let kerdesId = params['id'];
+    let kerdesSzovege = $(this).text();
+    console.log(kerdesSzovege);
+    let token = $("meta[name='csrf-token']").attr("content");
+    $.ajax({
+    url: '/getAnswers',    //kérdés válaszlehetőségeit és azokhoz tartozó információt kérjük le a route-on
+    type: 'GET',
+    data: {
+      'kerdesId' : kerdesId,
+      '_token': token,
+    },
+    success: function(response)
+    {
+      Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: kerdesSzovege
+        },
+        xAxis: {
+            categories: [
+              response.valaszok[0]['valasz'],
+              response.valaszok[1]['valasz'],
+              response.valaszok[2]['valasz'],
+              response.valaszok[3]['valasz'],
+              response.valaszok[4]['valasz']
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Válaszadók (fő)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} fő</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [
+          {
+            name: 'Fiatal',
+            
+            data: [
+              parseFloat(response.valaszok[0]['fiatalok']),
+              parseFloat(response.valaszok[1]['fiatalok']),
+              parseFloat(response.valaszok[2]['fiatalok']),
+              parseFloat(response.valaszok[3]['fiatalok']),
+              parseFloat(response.valaszok[4]['fiatalok'])
+            ]
+        },
+         {
+            name: 'Középkorú',
+            data: [
+              parseFloat(response.valaszok[0]['kozepkoruak']),
+              parseFloat(response.valaszok[1]['kozepkoruak']),
+              parseFloat(response.valaszok[2]['kozepkoruak']),
+              parseFloat(response.valaszok[3]['kozepkoruak']),
+              parseFloat(response.valaszok[4]['kozepkoruak'])
+            ]
+    
+        },
+         {
+            name: 'Idős',
+            data: [
+              parseFloat(response.valaszok[0]['idosek']),
+              parseFloat(response.valaszok[1]['idosek']),
+              parseFloat(response.valaszok[2]['idosek']),
+              parseFloat(response.valaszok[3]['idosek']),
+              parseFloat(response.valaszok[4]['idosek'])
+            ]
+    
+        },
+         ]
+    });
+      
+    }
+    });
+
+    token = $("meta[name='csrf-token']").attr("content2");
+    $.ajax({
+    url: '/getAnswers',    //kérdés válaszlehetőségeit és azokhoz tartozó információt kérjük le a route-on
+    type: 'GET',
+    data: {
+      'kerdesId' : kerdesId,
+      '_token': token,
+    },
+    success: function(response)
+    {
+      Highcharts.chart('container2', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: kerdesSzovege
+        },
+        
+        xAxis: {
+            categories: [
+              response.valaszok[0]['valasz'],
+              response.valaszok[1]['valasz'],
+              response.valaszok[2]['valasz'],
+              response.valaszok[3]['valasz'],
+              response.valaszok[4]['valasz']
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Válaszadók (fő)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} fő</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [
+          {
+            name: 'Férfiak',
+            
+            data: [
+              parseFloat(response.valaszok[0]['ferfi']),
+              parseFloat(response.valaszok[1]['ferfi']),
+              parseFloat(response.valaszok[2]['ferfi']),
+              parseFloat(response.valaszok[3]['ferfi']),
+              parseFloat(response.valaszok[4]['ferfi'])
+            ]
+        },
+         {
+            name: 'Nők',
+            data: [
+              parseFloat(response.valaszok[0]['no']),
+              parseFloat(response.valaszok[1]['no']),
+              parseFloat(response.valaszok[2]['no']),
+              parseFloat(response.valaszok[3]['no']),
+              parseFloat(response.valaszok[4]['no'])
+            ]
+    
+        },
+         {
+            name: 'Egyéb',
+            data: [
+              parseFloat(response.valaszok[0]['egyeb']),
+              parseFloat(response.valaszok[1]['egyeb']),
+              parseFloat(response.valaszok[2]['egyeb']),
+              parseFloat(response.valaszok[3]['egyeb']),
+              parseFloat(response.valaszok[4]['egyeb'])
+            ]
+    
+        },
+         ]
+    });
+      
+    }
+    });
+  })
   
 }
 
