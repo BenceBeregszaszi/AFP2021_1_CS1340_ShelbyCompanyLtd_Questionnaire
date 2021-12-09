19d968acae850a549603511c5e26e5d432c06359
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-12-09 14:44:52 +0100

    Statisztika ROUTE-ot lekezelő php function megírása

--- Questionnaire/app/Http/Controllers/PagesController.php
+++ Questionnaire/app/Http/Controllers/PagesController.php
@@ -7,18 +7,17 @@ use Illuminate\Support\Facades\DB;
 
 class PagesController extends Controller
 {
-    public function index() {
+    public function kitolt($id) {
         $numberOfQuestionnaire = DB::table('kerdoivs')->count('kerdoiv_id');
         if($numberOfQuestionnaire != 0)
         {
             $questionnaire = DB::table('kerdoivs')
             ->select('*')
-            ->orderby('kerdoiv_id')->first();
+            ->where('kerdoiv_id', '=',  $id)->first();
 
-            $questionnaire_id = $questionnaire->kerdoiv_id;
             $questions = DB::table('kerdeseks')
             ->select('*')
-            ->where('kerdoiv_id', '=',  $questionnaire_id)->get();
+            ->where('kerdoiv_id', '=',  $id)->get();
 
             $answers_arr = [];
             foreach($questions as $question)
@@ -29,15 +28,40 @@ class PagesController extends Controller
                 ->where('kerdes_id', '=', $question_id)->get();
                 array_push($answers_arr, $answers);
             }
-            return view('pages.index', compact('numberOfQuestionnaire'))
+            return view('pages.kitolto', compact('numberOfQuestionnaire'))
             ->with('questionnaire',$questionnaire)
             ->with('questions',$questions)
             ->with('answers_arr',$answers_arr);
         }
         else{
-            return view('pages.index', compact('numberOfQuestionnaire'));
+            return view('pages.kitolto', compact('numberOfQuestionnaire'));
         }
-       
     }
+    
+    public function charts() {
+            $questionnaires = DB::table('kerdoivs')
+            ->select('*')->get();
+
+            $questions = DB::table('kerdeseks')
+            ->select('*')->get();
+
+            $answers_arr = [];
+            foreach($questions as $question)
+            {
+                $question_id = $question->kerdes_id;
+                $answers = DB::table('valaszoks')
+                ->select('*')
+                ->where('kerdes_id', '=', $question_id)->get();
+                array_push($answers_arr, $answers);
+            }
+            return view('pages.highcharts')
+            ->with('questionnaires',$questionnaires)
+            ->with('questions',$questions)
+            ->with('answers_arr',$answers_arr);
+    }
+    
+    public function index() {
+        $numberOfQuestionnaire = DB::table('kerdoivs')->count('kerdoiv_id');
+        return view('pages.index')->with('numberOfQuestionnaire', $numberOfQuestionnaire);
 }
         
