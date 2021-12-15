8a6e235a13dc46f1f3f744d1e42dc3b65f19984d
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2021-12-15 19:29:40 +0100

    Highcharts js tagek importálása (app.blade.php header)

--- Questionnaire/resources/views/layouts/app.blade.php
+++ Questionnaire/resources/views/layouts/app.blade.php
@@ -15,6 +15,11 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+    <!-- Highcharts -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
     <title>@yield('title')</title>
 
 </head>
