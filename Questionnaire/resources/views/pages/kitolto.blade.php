36bc0eb8f81d1000551900cff082080bfc24c568
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2021-12-15 18:34:02 +0100

    kitolto.blade.php újra formázása

--- Questionnaire/resources/views/pages/kitolto.blade.php
+++ Questionnaire/resources/views/pages/kitolto.blade.php
@@ -8,54 +8,42 @@
 
         <div class="flex justify-center @if ($numberOfQuestionnaire < 1) w-full @endif">
             
-            <div class="shadow-lg font-light bg-gray-100 p-7 rounded-lg content-center px-10"> <!--width 3 out of 4, background white, padding 6, rounded edges-->
+            <div class="shadow-lg font-light bg-gray-100 p-5 rounded-lg content-center px-5">
                 @if ($numberOfQuestionnaire < 1)
-                    <p class="w-full p-5 text-lg lg:text-3xl text-center">Jelenleg nincs kérdés az adatbázisban!</p>
+                    <p class="w-full text-lg lg:text-3xl text-center">Jelenleg nincs kérdés az adatbázisban!</p>
                 @else
-                    <p class="text-4xl text-center">{{ $questionnaire->kerdoiv_nev }}</p>
-                    <form action="/send" method="POST" class="px-10 py-5 bg-gray-100 rounded-lg my-5">
+                    <p class="text-6xl text-center font-bold">{{ $questionnaire->kerdoiv_nev }}</p>
+                    <form action={{ asset('send') }} method="POST" class="md:px-10 px-2 py-5 bg-gray-100 rounded-lg my-5">
                         @csrf
                         
-                        <table class="m-5 p-2 table-auto">
-                            <thead>
-                                <td class="text-2xl">Neme:</td>
-                                <td class="text-2xl pl-5">Életkora:</td>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <input type="radio" name="neme" id="ferfi" value="Férfi">
-                                        <label class="pl-2 text-xl" for="ferfi">Férfi</label>
-                                    </td>
-                                    <td>
-                                        <input type="number" name="eletkora" class="w-15 px-2 py-3 rounded bg-white m-5">
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input type="radio" name="neme" id="no" value="Nő">
-                                        <label class="pl-2 text-xl" for="no" class="text-3xl mb-5">Nő</label>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input type="radio" name="neme" id="egyeb" value="Egyéb">
-                                        <label class="pl-2 text-xl" for="egyeb" class="text-3xl mb-5">Egyéb</label>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <br>
+                        <div class="row space-x-10">
+                            <div class="colun">
+                                <label for="neme" class="text-2xl font-medium">Neme:</label><br>
+                                <input type="radio" name="neme" id="ferfi" value="Férfi">
+                                <label class="pl-2 text-xl" for="ferfi">Férfi</label><br>
+
+                                <input type="radio" name="neme" id="no" value="Nő">
+                                <label class="pl-2 text-xl" for="no" class="text-3xl mb-5">Nő</label><br>
+                                                
+                                <input type="radio" name="neme" id="egyeb" value="Egyéb">
+                                <label class="pl-2 text-xl" for="egyeb" class="text-3xl mb-5">Egyéb</label>
+                            </div>
+                            <div  class="column">
+                                <label for="eletkore" class="text-2xl font-medium">Életkora:</label>
+                                <input type="number" name="eletkora" class="w-15 px-2 py-3 rounded bg-white m-5">
+                            </div>
+                        </div>
+                                    
                         <hr class="pt-5">
-                        <br>
+
                         @foreach ($questions as $question)
                             <div class="mb-5">
-                                <p class="text-2xl mb-3">{{ $question->kerdes_szovege }}</p>
+                                <p class="text-2xl mb-3 font-medium">{{ $question->kerdes_szovege }}</p>
                                 @foreach ($answers_arr as $answers)
                                     @foreach ($answers as $answer)
                                         @if($answer->kerdes_id == $question->kerdes_id)
                                             <input type="radio" name="kerdes{{ $question->kerdes_id }}" id="{{ $answer->valaszok_id }}" value="{{ $answer->valasz }}">
-                                            <label for="{{ $answer->valaszok_id }}" class="text-lg pl-3">{{ $answer->valasz }}</label><br>
+                                            <label for="{{ $answer->valaszok_id }}" class="text-lg pl-3 font-light">{{ $answer->valasz }}</label><br>
                                         @endif
                                     @endforeach
                                 @endforeach
