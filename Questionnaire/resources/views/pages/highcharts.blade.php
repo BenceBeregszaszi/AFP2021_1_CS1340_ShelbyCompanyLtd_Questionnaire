2192c007fafc904922dadcbe33ab81a22cf30b35
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-12-15 11:02:54 +0100

    Statisztika oldal autentikáció vizsgálat hozzáadás

--- Questionnaire/resources/views/pages/highcharts.blade.php
+++ Questionnaire/resources/views/pages/highcharts.blade.php
@@ -3,22 +3,35 @@
 @section('title') {{'Statisztika'}} @endsection
 
 @section('content')
+
+@if (Auth::check())
+
+  @if (Session::has('message'))
+    <div class="flex justify-center font-sans text-center">
+      <div class="w-full md:w-1/2 mb-10">                     
+        <div class="px-5 text-2xlfont-light">{{ Session::get('message') }}</div>
+      </div>
+    </div>
+  @endif
 <div class="flex justify-center" style="position: fixed;">
   <div class="w-screen row">
-      <div style="overflow: auto;" class="column bg-gray-50 rounded-lg mt-12 ml-12 shadow-lg h-96	" id="felsorolas">
+      <div style="overflow: auto; height: 65%" class="column bg-gray-50 rounded-lg mt-8 ml-6 shadow-lg" id="felsorolas">
                 @foreach ($questions as $question)
                         <div class="questionBtn text-2xl" data-id="{{ $question->kerdes_id }}">
                           {{ $question->kerdes_szovege }}
                         </div> 
                 @endforeach
       </div>
-      <div class="column text-2xl mt-12 ml-3 h-96" style="width: 30%;">
+      <div class="column text-2xl mt-8 ml-3" style="width: 30%;">
           <div id="container" style="width: 100%; height: 100%"></div>
       </div>   
-      <div class="column text-2xl mt-12 h-96" style="width: 30%;">  
+      <div class="column text-2xl mt-8" style="width: 30%;">  
         <div id="container2" style="width: 100%; height: 100%"></div>
       </div>
+      
   </div>
 </div>
 
+
+@endif
 @endsection
