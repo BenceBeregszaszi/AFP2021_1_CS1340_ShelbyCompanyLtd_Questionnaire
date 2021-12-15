9e38098e0d8a279edd70c49d8ed0a17027521a9e
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2021-12-15 19:55:36 +0100

    Update oldal módosítása jquery function-höz

--- Questionnaire/resources/views/pages/update.blade.php
+++ Questionnaire/resources/views/pages/update.blade.php
@@ -11,7 +11,7 @@
             <h1 class="text-3xl font-light">
                 <b>{{ $question->kerdes_szovege }}</b> kérdés válaszai
             </h1>
-            <button class="mt-5 px-3 py-2 text-white font-light text-xl rounded-lg shadow-lg bg-green-600 transition hover:bg-green-400" id="changerButton">Kérdés módosítása</button>
+            <button id="changerButton">Változtat</button>
     </div>
     <h2 class="text-center" style="color: red;" id ="response"></h2>
     <div class="text-center" id="cimChanger" style="display: none">
@@ -20,7 +20,7 @@
         <input type="hidden" value="{{ $question->kerdes_id }}" name="idToChange" id = "hiddenQuestionId">
         <input type="hidden" value="kerdes" name="whatToChange">
         <input type="text" value = "{{ $question->kerdes_szovege }}" name="ujNev" style="font-size: 32px">
-        <button type="submit" class="flex justify-center mt-5 bg-green-600 rounded-md w-1/6 m-auto py-2 px-2 text-gray-100 text-xl font-light transition hover:bg-green-500">Frissít</button>
+        <button type="submit" class="flex justify-center bg-green-600 rounded-md w-1/6 m-auto py-2 px-2 text-gray-100 text-xl font-light transition hover:bg-green-500">Frissít</button>
       </form>
   
     </div>
@@ -40,7 +40,7 @@
                 @forelse ($answers as $answer)
                 <tr class="pb-10 font-light">
                   <td>
-                    <input type="text" class="InputProbaUpdate" data-id = "{{ $answer->valaszok_id }}" name="szoveg{{ $answer->valaszok_id }}" value="{{ $answer->valasz }}" class="rounded mt-5 mb-3 p-3 text" size="80" required>
+                    <input type="text" class="InputUpdate" data-id = "{{ $answer->valaszok_id }}" name="szoveg{{ $answer->valaszok_id }}" value="{{ $answer->valasz }}" class="rounded mt-5 mb-3 p-3 text" size="80" required>
                     <br>
                     <hr class="pt-5">
                   </td>
@@ -49,7 +49,7 @@
                 @endforelse
               </tbody>
             </table>
-            <button type="button"  class="flex justify-center bg-green-600 rounded-md w-1/3 m-auto py-2 px-2 text-gray-100 text-xl font-light transition hover:bg-green-500" id="ButtonProbaUpdate">Módosít</button>
+            <button type="button"  class="flex justify-center bg-green-600 rounded-md w-1/3 m-auto py-2 px-2 text-gray-100 text-xl font-light transition hover:bg-green-500" id="ButtonUpdate">Módosít</button>
           </div>
       </div>
 
